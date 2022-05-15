3c3e76173c9e3a2a893bd8dc0b1a3a628d83d2b3
user <tomi.segev@gmail.com>
2022-05-15 15:18:28 +0300

    exercise 4

--- answers.js
+++ answers.js
@@ -9,4 +9,11 @@
 // let car = {firstname:"Tomer", lastname:"Segev", age:19, height:"190cm", weight:"80kg", habit:"basketball", musicalinstrument:"guitar", favicecream:"vanilla", livingin:"Holon", fiveunits:"Arabic"}
 
 //exercise4
-let number
+let number = 4
+number *= 4
+number--
+number /= 12
+number % 3
+for (let index = 0; index < 6; index++) {
+    console.log(number)
+}
