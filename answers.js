7bf8807f9197ff969b01e151d7214fa74e84f02d
user <tomi.segev@gmail.com>
2022-05-15 15:24:35 +0300

    exercise 5

--- answers.js
+++ answers.js
@@ -9,11 +9,23 @@
 // let car = {firstname:"Tomer", lastname:"Segev", age:19, height:"190cm", weight:"80kg", habit:"basketball", musicalinstrument:"guitar", favicecream:"vanilla", livingin:"Holon", fiveunits:"Arabic"}
 
 //exercise4
-let number = 4
-number *= 4
-number--
-number /= 12
-number % 3
-for (let index = 0; index < 6; index++) {
-    console.log(number)
+// let number = 4
+// number *= 4
+// number--
+// number /= 12
+// number % 3
+// for (let index = 0; index < 6; index++) {
+//     console.log(number)
+// }
+
+// exercise5
+let age = 24
+if (age === 23) {
+    console.log("congratulations")
+}
+else if (age < 23) {
+    console.log("young")
+}
+else {
+    console.log("old")
 }
