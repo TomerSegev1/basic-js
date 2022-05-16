930731c98bb31138cf97c7f7ac4588ddef745675
user <tomi.segev@gmail.com>
2022-05-16 11:34:52 +0300

    exercise 2.3

--- another.js
+++ another.js
@@ -7,10 +7,27 @@
 // console.log(sum)
 
 // exercise2
-let num = 1234; cum = 1;
-while (num > 0)
-{
-    cum = cum * (num % 10)
-    cum = Math.trunc(num / 10)
+// let num = 1234; cum = 1;
+// while (num > 0)
+// {
+//     cum = cum * (num % 10)
+//     cum = Math.trunc(num / 10)
+// }
+// console.log(cum)
+
+//exercise3
+let mane = "nnnnnnn"
+let length = mane.length;
+let isGreat = false;
+for (index = 0; index <= length; index++) {
+    if (mane.charAt(index) === "m" || mane.charAt(index) === "i" || mane.charAt(index) === "d" || mane.charAt(index) === "b" || mane.charAt(index) === "a" || mane.charAt(index) === "r") {
+        isGreat = true;
+    }
 }
-console.log(cum)
+
+if (isGreat) {
+    console.log("great");
+}
+else {
+    console.log("bad");
+}
