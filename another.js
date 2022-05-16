10404e006cad8e3281aab4e3c369881c51693b3e
user <tomi.segev@gmail.com>
2022-05-16 10:08:04 +0300

    exercise 2.2

--- another.js
+++ another.js
@@ -1,7 +1,16 @@
 // exercise1
-let a = 5; b = 6; sum = 0;
-for(index = b; index > 0; index --)
+// let a = 5; b = 6; sum = 0;
+// for(index = b; index > 0; index --)
+// {
+//     sum = sum + a
+// }
+// console.log(sum)
+
+// exercise2
+let num = 1234; cum = 1;
+while (num > 0)
 {
-    sum = sum + a
+    cum = cum * (num % 10)
+    cum = Math.trunc(num / 10)
 }
-console.log(sum)
+console.log(cum)
